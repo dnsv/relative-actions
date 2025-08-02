67f0a7cc57ecda590271170dc950e6f1032c24a6
Dejan Svetec <dejan.svetec@icloud.com>
2025-08-02 22:35:02 +0200

    Temporarily highlight copied text

--- src/main/kotlin/com/github/dnsv/relativeactions/relativeaction/CopyRelativeAction.kt
+++ src/main/kotlin/com/github/dnsv/relativeactions/relativeaction/CopyRelativeAction.kt
@@ -2,18 +2,64 @@ package com.github.dnsv.relativeactions.relativeaction
 
 import com.github.dnsv.relativeactions.util.LineRange
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.HighlighterLayer
+import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.util.TextRange
 import java.awt.datatransfer.StringSelection
+import javax.swing.Timer
 
 class CopyRelativeAction(
     editor: Editor,
     lineRange: LineRange,
 ) : AbstractRelativeAction(editor, lineRange) {
+    companion object {
+        private const val HIGHLIGH_DURATION_MS = 150
+    }
+
     override fun execute() {
         val (startOffset, endOffset) = getLineOffsets()
         val selection = StringSelection(getDocument().getText(TextRange(startOffset, endOffset)))
 
         CopyPasteManager.getInstance().setContents(selection)
+
+        highlightCopiedText(startOffset, endOffset)
+    }
+
+    /**
+     * Provides visual feedback by temporarily highlighting the copied text.
+     *
+     * @param startOffset The start offset of the copied text.
+     * @param endOffset The end offset of the copied text.
+     */
+    private fun highlightCopiedText(
+        startOffset: Int,
+        endOffset: Int,
+    ) {
+        val scheme = EditorColorsManager.getInstance().globalScheme
+        val attributes =
+            TextAttributes().apply {
+                backgroundColor = scheme.getColor(EditorColors.SELECTION_BACKGROUND_COLOR)
+                foregroundColor = scheme.getColor(EditorColors.SELECTION_FOREGROUND_COLOR)
+            }
+        val highlighter =
+            editor.markupModel.addRangeHighlighter(
+                startOffset,
+                endOffset,
+                HighlighterLayer.SELECTION,
+                attributes,
+                HighlighterTargetArea.EXACT_RANGE,
+            )
+
+        // Remove highlight after a short delay
+        Timer(HIGHLIGH_DURATION_MS) {
+            editor.markupModel.removeHighlighter(highlighter)
+        }.apply {
+            isRepeats = false
+            start()
+        }
     }
 }
--- src/main/kotlin/com/github/dnsv/relativeactions/util/Notify.kt
+++ src/main/kotlin/com/github/dnsv/relativeactions/util/Notify.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 
 object Notify {
-    const val NOTIFICATION_TITLE = "Relative Actions"
+    private const val NOTIFICATION_TITLE = "Relative Actions"
 
     private val notificationGroup = NotificationGroupManager.getInstance().getNotificationGroup("relative-actions")
 
